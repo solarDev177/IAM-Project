5a5f644d570dd1db29bde04644198db42185cd0e
Solar <Tys876@proton.me>
2026-04-11 18:33:32 -0500

    Packaging:
    Added Windows installer.
    
    Backout process: last version
    
    Risk: low

--- build_windows_installer.ps1
+++ build_windows_installer.ps1
@@ -27,6 +27,7 @@ function Find-InnoSetupCompiler {
     # Locate the Inno Setup compiler in the common Windows install paths.
     $candidatePaths = @(
         $env:INNO_SETUP_COMPILER,
+        (Join-Path $env:LOCALAPPDATA "Programs\Inno Setup 6\ISCC.exe"),
         "C:\Program Files (x86)\Inno Setup 6\ISCC.exe",
         "C:\Program Files\Inno Setup 6\ISCC.exe"
     ) | Where-Object { $_ }
